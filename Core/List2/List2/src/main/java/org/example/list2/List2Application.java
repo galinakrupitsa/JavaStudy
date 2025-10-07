af6b2254cd28942996ecf15af912f00119418683
galina <galina.krupitsa@gmail.com>
2025-10-07 12:43:33 +0500

    f

---
+++ Core/List2/List2/src/main/java/org/example/list2/List2Application.java
@@ -0,0 +1,14 @@
+package org.example.list2;
+import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class List2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(List2Application.class, args);
+    }
+
+
+}
