e8a47dd167be70053706305bddbdef44b6f5e988
galina <galina.krupitsa@gmail.com>
2025-10-08 15:45:35 +0500

    p

---
+++ Spring/LearnSet/LearnSet/src/main/java/org/example/learnset/LearnSetApplication.java
@@ -0,0 +1,13 @@
+package org.example.learnset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSetApplication.class, args);
+    }
+
+}
