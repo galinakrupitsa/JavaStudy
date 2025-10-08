96066bb35e4c463ea9233a8cb1a2b5c9f841a810
galina <galina.krupitsa@gmail.com>
2025-10-08 17:12:56 +0500

    h

---
+++ Spring/LearnSet/LearnSet/src/main/java/org/example/learnset/DTO.java
@@ -0,0 +1,15 @@
+package org.example.learnset;
+
+
+public class DTO {
+    public String name;
+//    public DTO(String name) {
+//        this.name = name;
+//    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- Spring/LearnSet/LearnSet/src/main/java/org/example/learnset/SetController.java
+++ Spring/LearnSet/LearnSet/src/main/java/org/example/learnset/SetController.java
@@ -1,8 +1,28 @@
 package org.example.learnset;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RestController
-public class SetController {
 
-}
+public class SetController {
+    public HashSet<String> set = new HashSet<>(List.of("df","dfdf","jkj","Alex"));
+    public DTO dto = new DTO();
+
+    @GetMapping("/hascustomer")
+
+    public String hasCustomer(@RequestBody DTO dto) {
+        if(set.contains(dto.getName())) {
+            return "true";
+        }
+            else return "false";
+        }
+
+
+    }
+
+
+
