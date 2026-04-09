a2010821a1dca8ad00be9ff2aed0ebd22b8f5494
galina <galina.krupitsa@gmail.com>
2026-04-09 21:58:35 +0500

    bubble

--- Core/Sort/Sort/src/main/java/Main.java
+++ Core/Sort/Sort/src/main/java/Main.java
@@ -1,11 +1,22 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {3, 4, 2, 8, 1};
+        int[] array = {8, 4, 2, 3, 1};
+        int target = 7;
 
         System.out.println(Arrays.toString(array));
+        System.out.println("Метод пузырька");
         bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println("нахождение суммы " + target);
+        int[] result = findTwoIndices(array, target);
+        if (result.length == 2) {
+            System.out.println("Индексы чисел: " + Arrays.toString(result));
+        } else {
+            System.out.println("Пара не найдена.");
+        }
     }
         public static void bubbleSort(int[] array){
         int n = array.length;
@@ -28,7 +39,22 @@ public class Main {
         System.out.println(Arrays.toString(array));
     }
 
+    // Метод возвращает массив из двух индексов
+    public static int[] findTwoIndices(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>(); // хранит число -> индекс
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i}; // возвращаем индексы
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1}; // если пара не найдена
+    }
+
 }
+
+
 
 
 
